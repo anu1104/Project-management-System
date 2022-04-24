eb8a516b88e79783e7c146384ec3a91bc996a2f0
DhananjayDJ010 <dhananjayj010@gmail.com>
2022-04-24 17:08:42 +0530

    added sprint model to user service

---
+++ user-service/src/main/java/com/projectmanagementsystem/userservice/model/SprintModel.java
@@ -0,0 +1,20 @@
+package com.projectmanagementsystem.userservice.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SprintModel {
+    private String name;
+    private int projectId;
+    private String duration;
+    private Date startDate;
+    private Date endDate;
+    private boolean isSprintActive;
+
+}
